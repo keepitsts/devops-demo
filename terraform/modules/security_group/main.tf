fa996615e6329c94c874050759612975d3ec7b73
CloudDevOpsEngineer <anthonyblutz@gmail.com>
2019-06-28 08:25:34 -0400

    updated vpc, subnet, and other values

--- terraform/modules/security_group/main.tf
+++ terraform/modules/security_group/main.tf
@@ -8,30 +8,56 @@ resource "aws_security_group" "security_group" {
   vpc_id      = "${var.vpc_id}"
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = "${var.http_cidr}"
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = "${var.http_cidr}"
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = "${var.ssh_cidr}"
+  # TLS (change to whatever ports you need)
+  from_port   = 443
+  to_port     = 443
+
+  protocol    = "tcp"
+  # Please restrict your ingress to only necessary IPs and ports.
+  # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+  cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
+  ingress {
+  # TLS (change to whatever ports you need)
+  from_port   = 80
+  to_port     = 80
+  
+  protocol    = "tcp"
+  # Please restrict your ingress to only necessary IPs and ports.
+  # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+  cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+  # TLS (change to whatever ports you need)
+  from_port   = 8080
+  to_port     = 8080
+  
+  protocol    = "tcp"
+  # Please restrict your ingress to only necessary IPs and ports.
+  # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+  # TLS (change to whatever ports you need)
+  from_port   = 22
+  to_port     = 22 
+  
+  protocol    = "tcp"
+  # Please restrict your ingress to only necessary IPs and ports.
+  # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   tags = {
     Name = "${var.sg_name}"
   }
